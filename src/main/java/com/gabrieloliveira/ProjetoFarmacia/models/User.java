d28ebab3715c63f9058c91fc03d4eb930a55384b
Gabriel Oliveira <gdravk@outlook.com>
2024-03-21 09:20:02 -0300

    criado modelo de usuario

--- src/main/java/com/gabrieloliveira/ProjetoFarmacia/models/User.java
+++ src/main/java/com/gabrieloliveira/ProjetoFarmacia/models/User.java
@@ -10,7 +10,10 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 
 @Entity
 @Table(name = User.TABLE_NAME)
@@ -33,7 +36,8 @@ public class User {
     @Size(groups = CreateUser.class, min = 6, max = 60)
     private String username;
 
-    @Column(name = "username", length = 60, nullable = false)
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Column(name = "password", length = 60, nullable = false)
     @NotEmpty(groups = {CreateUser.class, UpdateUser.class}) //empty mesma coisa q o not Null e not empty, ele faz os dois ao mesmo tempo
     @Size( groups = {CreateUser.class, UpdateUser.class}, min = 6, max = 60)
     private String password;
@@ -42,13 +46,6 @@ public class User {
 
 
     public User() {
-    }
-    
-
-    public User(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
     }
 
 
@@ -76,5 +73,45 @@ public class User {
         this.password = password;
     }
 
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        return true;
+    }
+
     
+
 }
